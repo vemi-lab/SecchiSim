a31b34fb0988c6139510b754b41eca780d3a1357
Alexa Guerrero <alexa.guerrero@maine.edu>
2025-01-31 13:39:09 -0500

    readjust

--- sim/src/components/App.js
+++ sim/src/components/App.js
@@ -3,13 +3,6 @@ import Signup from './Signup';
 import Login from './Login'
 import ForgotPassword from './ForgotPassword';
 import Dashboard from './Dashboard';
-import TryItOut from './disk-sim/TryItOut';
-import Instructions from './disk-sim/Instructions';
-import ClearLake from './disk-sim/LakeTypes/ClearLake';
-import IntermediateLake from './disk-sim/LakeTypes/IntermediateLake';
-import ProductiveLake from './disk-sim/LakeTypes/ProductiveLake';
-import DystrophicLake from './disk-sim/LakeTypes/DystrophicLake';
-import DystrophicProductiveLake from './disk-sim/LakeTypes/DystrophicProductiveLake';
 import UpdateProfile from './UpdateProfile';
 import PrivateRoute from './PrivateRoute'
 import {Container} from 'react-bootstrap';
@@ -29,13 +22,6 @@ function App() {
               <Route path="/forgot-password" element={<ForgotPassword />} />
               <Route exact path="/dashboard" element={<PrivateRoute component={Dashboard}/>} />
               <Route exact path="/update-profile" element={<PrivateRoute component={UpdateProfile}/>} />
-              <Route exact path="/try-it-out" element={<PrivateRoute component={TryItOut}/> } />
-              <Route exact path="/instructions" element={<PrivateRoute component={Instructions}/> } />
-              <Route path="/lake/clear" element={<PrivateRoute component={ClearLake}/> } />
-              <Route path="/lake/intermediate" element={<PrivateRoute component={IntermediateLake}/> } />
-              <Route path="/lake/productive" element={<PrivateRoute component={ProductiveLake}/> } />
-              <Route path="/lake/dystrophic" element={<PrivateRoute component={DystrophicLake}/> } />
-              <Route path="/lake/dystrophic-productive" element={<PrivateRoute component={DystrophicProductiveLake}/> } />
             </Routes>
           </div> 
         </Container>
