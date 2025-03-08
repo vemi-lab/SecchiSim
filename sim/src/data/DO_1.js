f7ec714bfbfe98570a86efd643af5e11eedc9826
pgalleg03 <paula.gallego@maine.edu>
2025-03-08 17:47:31 -0500

    These are the files with the quiz data

---
+++ sim/src/data/DO_1.js
@@ -0,0 +1,65 @@
+const data = [
+    {
+      question: "Why do we take dissolved oxygen readings in lakes? Please select all that are true.",
+      options: [
+        "a. Because it helps us understand the impacts of polluted stormwater runoff.",
+        "b. Because it allows us to understand the conductivity of the waterbody.",
+        "c. Because it helps us understand the stressors on biotic life, especially coldwater fish."
+      ],
+      correct_option: ["a. Because it helps us understand the impacts of polluted stormwater runoff.", 
+        "c. Because it helps us understand the stressors on biotic life, especially coldwater fish."]
+    },
+    {
+      question: "Which of the following represents the greatest threat to the health of Maine's lakes?",
+      options: [
+        "Lead",
+        "Nitrogen",
+        "Phosphorus",
+        "Strontium",
+        "Carbon"
+      ],
+      correct_option: ["Phosphorus"]
+    },
+    {
+      question: "Which of these are potential sources of phosphorus? Please select all that are true.",
+      options: [
+        "Soil",
+        "Fertilizer",
+        "Animal waste",
+        "Septic systems (especially failures)",
+        "Decaying organic matter"
+      ],
+      correct_option: ["Soil", "Fertilizer", "Animal waste", "Septic systems (especially failures)", "Decaying organic matter"]
+    },
+    {
+      question: "Why does a forested watershed experience less stormwater runoff than even a low-density development?",
+      options: [
+        "The forest leaves intercept some of the precipitation and slow down its hitting the ground.",
+        "Rainfall is slowed down by irregularities in the forest floor, where infiltration and evaporation take place.",
+        "Lawns typically do not absorb precipitation as effectively as wooded areas.",
+        "Even low-density developed areas have more impervious area than forested watersheds.",
+        "All of the above."
+      ],
+      correct_option: ["All of the above."]
+    },
+    {
+      question: "What is thermal stratification?",
+      options: [
+        "The formation of distinct temperature layers in the lake water column of lakes with moderate depth.",
+        "The effect of Secchi disk readings on lake water temperature.",
+        "Both are correct."
+      ],
+      correct_option: ["The formation of distinct temperature layers in the lake water column of lakes with moderate depth."]
+    },
+    {
+      question: "What is the appropriate unit of measurement for dissolved oxygen on your data sheet? Please select all that are true.",
+      options: [
+        "Milligrams per liter (mg/L)",
+        "Percent saturation (%DO)"
+      ],
+      correct_option: ["Milligrams per liter (mg/L)"]
+    }
+  ];
+
+  export default data
+  
---
+++ sim/src/data/Secchi_1.js
@@ -0,0 +1,51 @@
+const data = [
+    {
+        question: "What is the primary mission/role of Lake Stewards of Maine?",
+        options: [
+            "To identify specific sources of pollution on lakes.", 
+            "To engage and train the public in the gathering of credible lake data pertaining to the health of Maine lakes.", 
+            "To assist state agencies in the enforcement of lake protection standards."],
+        correct_option: ["To engage and train the public in the gathering of credible lake data pertaining to the health of Maine lakes."]
+    },
+    {
+        question: "Why is citizen/community lake science valuable to Maine’s lakes?",
+        options: [
+            "With several thousand Maine lakes in public domain, the only feasible way to gather sufficient data to help understand and protect our lakes is through training interested members of the public to gather lake data over time.",
+            "Because state and federal agencies do not have sufficient financial and human resources to visit all public lakes on a frequent basis.",
+            "Because lake water quality data is useful for policy decisions concerning lakes.",
+            "All of the above",
+            "a and c only"
+        ],
+        correct_option: ["All of the above"]
+    },
+    {
+        question: "Who can volunteer to become an LSM steward/lake monitor?",
+        options: [
+            "Only individuals with a strong technical background in science.",
+            "Only adults between the ages of 20 and 60 years.",
+            "Anyone who has an interest in the health of Maine’s lakes, and is willing to undergo a training and certification process provided by LSM."
+        ],
+        correct_option: ["Anyone who has an interest in the health of Maine’s lakes, and is willing to undergo a training and certification process provided by LSM."]
+    },
+    {
+        question: "What is the cost for becoming a certified LSM lake monitor?",
+        options: [
+            "The initial training fee is $50, after which, the cost of periodic recertification is $25.",
+            "No fee is required to participate in the organization.",
+            "The annual cost is dependent on the experience of each individual."
+        ],
+        correct_option: ["No fee is required to participate in the organization."]
+    },
+    {
+        question: "What forms of training are provided to interested volunteers through LSM?",
+        options: [
+            "Lake water quality monitoring.",
+            "Information on how to conduct an invasive aquatic species survey.",
+            "Training for conducting lake watershed surveys.",
+            "All of the above."
+        ],
+        correct_option: ["All of the above."]
+    }
+];
+
+export default data;
